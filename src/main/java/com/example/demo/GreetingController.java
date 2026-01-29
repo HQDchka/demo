8cc72be7e3e10d00a6f1d9feb2f8873c1c44191c
Daniil Gaponov(Даниил Гапонов) <Daniilgaponov05@gmail.com>
2026-01-29 13:58:16 +0300

    Hello, "name" + "surname"
    http://localhost:8080/greet/full?name=John&surname=Smith

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,7 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
     @GetMapping
-    public String greet() {
-        return "Hello";
+    public String greet(@RequestParam("name") String name) {
+        return "Hello, " + name;
     }
+
+    @GetMapping("/full")
+    public String fullGreeting(@RequestParam("name") String name,
+                               @RequestParam("surname") String surname) {
+        return "Nice to meet you, " + name + " " + surname;
+    }
+
 }
