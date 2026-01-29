05562b5808e88cb9c1a98d7152794979538e8cfc
Daniil Gaponov(Даниил Гапонов) <Daniilgaponov05@gmail.com>
2026-01-29 14:10:04 +0300

    реализация PathVariable
    RequestParam – http://localhost:8080/greet/full?name=John&surname=Smith
    PathVariable – http://localhost:8080/greet/John. В этом случае John является PathVariable.

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -1,9 +1,6 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/greet")
@@ -15,9 +12,13 @@ public class GreetingController {
     }
 
     @GetMapping("/full")
-    public String fullGreeting(@RequestParam("name") String name,
-                               @RequestParam("surname") String surname) {
+    public String fullGreeting(@RequestParam("name") String name, @RequestParam("surname") String surname) {
         return "Nice to meet you, " + name + " " + surname;
+    }
+
+    @GetMapping("/{name}")
+    public String greetWithPathVariable(@PathVariable("name") String name) {
+        return "Hello, " + name;
     }
 
 }
